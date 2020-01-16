a550d32a889f0bd87b9f7edd1da37f0822f1ecbc
Rezky Febrian <rezkyfebrian0299@gmail.com>
2020-01-16 21:46:14 +0700

    rezky menambahkan desain admin

---
+++ Visual Programming/TokoKlontong/TokoKlontong/admin.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class admin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.tbidadmin = New System.Windows.Forms.TextBox()
+        Me.tbusername = New System.Windows.Forms.TextBox()
+        Me.tbpassword = New System.Windows.Forms.TextBox()
+        Me.tbalamat = New System.Windows.Forms.TextBox()
+        Me.tbnotelp = New System.Windows.Forms.TextBox()
+        Me.bttambah = New System.Windows.Forms.Button()
+        Me.btedit = New System.Windows.Forms.Button()
+        Me.bthapus = New System.Windows.Forms.Button()
+        Me.dataadmin = New System.Windows.Forms.DataGridView()
+        CType(Me.dataadmin, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label1.Location = New System.Drawing.Point(12, 56)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ID Admin"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label2.Location = New System.Drawing.Point(12, 81)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "User Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label3.Location = New System.Drawing.Point(12, 110)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(69, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Password"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label4.Location = New System.Drawing.Point(12, 137)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(51, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Alamat"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label5.Location = New System.Drawing.Point(12, 201)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(73, 15)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "No Telpon"
+        '
+        'tbidadmin
+        '
+        Me.tbidadmin.Location = New System.Drawing.Point(105, 55)
+        Me.tbidadmin.Name = "tbidadmin"
+        Me.tbidadmin.Size = New System.Drawing.Size(266, 20)
+        Me.tbidadmin.TabIndex = 5
+        '
+        'tbusername
+        '
+        Me.tbusername.Location = New System.Drawing.Point(105, 80)
+        Me.tbusername.Name = "tbusername"
+        Me.tbusername.Size = New System.Drawing.Size(266, 20)
+        Me.tbusername.TabIndex = 6
+        '
+        'tbpassword
+        '
+        Me.tbpassword.Location = New System.Drawing.Point(105, 109)
+        Me.tbpassword.Name = "tbpassword"
+        Me.tbpassword.Size = New System.Drawing.Size(266, 20)
+        Me.tbpassword.TabIndex = 7
+        '
+        'tbalamat
+        '
+        Me.tbalamat.Location = New System.Drawing.Point(105, 136)
+        Me.tbalamat.Multiline = True
+        Me.tbalamat.Name = "tbalamat"
+        Me.tbalamat.Size = New System.Drawing.Size(266, 56)
+        Me.tbalamat.TabIndex = 8
+        '
+        'tbnotelp
+        '
+        Me.tbnotelp.Location = New System.Drawing.Point(105, 198)
+        Me.tbnotelp.Name = "tbnotelp"
+        Me.tbnotelp.Size = New System.Drawing.Size(266, 20)
+        Me.tbnotelp.TabIndex = 9
+        '
+        'bttambah
+        '
+        Me.bttambah.Location = New System.Drawing.Point(393, 81)
+        Me.bttambah.Name = "bttambah"
+        Me.bttambah.Size = New System.Drawing.Size(75, 23)
+        Me.bttambah.TabIndex = 10
+        Me.bttambah.Text = "&Tambah"
+        Me.bttambah.UseVisualStyleBackColor = True
+        '
+        'btedit
+        '
+        Me.btedit.Location = New System.Drawing.Point(393, 129)
+        Me.btedit.Name = "btedit"
+        Me.btedit.Size = New System.Drawing.Size(75, 23)
+        Me.btedit.TabIndex = 11
+        Me.btedit.Text = "&Edit"
+        Me.btedit.UseVisualStyleBackColor = True
+        '
+        'bthapus
+        '
+        Me.bthapus.Location = New System.Drawing.Point(393, 181)
+        Me.bthapus.Name = "bthapus"
+        Me.bthapus.Size = New System.Drawing.Size(75, 23)
+        Me.bthapus.TabIndex = 12
+        Me.bthapus.Text = "&Delete"
+        Me.bthapus.UseVisualStyleBackColor = True
+        '
+        'dataadmin
+        '
+        Me.dataadmin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dataadmin.Location = New System.Drawing.Point(15, 253)
+        Me.dataadmin.Name = "dataadmin"
+        Me.dataadmin.Size = New System.Drawing.Size(559, 150)
+        Me.dataadmin.TabIndex = 13
+        '
+        'admin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(589, 413)
+        Me.Controls.Add(Me.dataadmin)
+        Me.Controls.Add(Me.bthapus)
+        Me.Controls.Add(Me.btedit)
+        Me.Controls.Add(Me.bttambah)
+        Me.Controls.Add(Me.tbnotelp)
+        Me.Controls.Add(Me.tbalamat)
+        Me.Controls.Add(Me.tbpassword)
+        Me.Controls.Add(Me.tbusername)
+        Me.Controls.Add(Me.tbidadmin)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "admin"
+        Me.Text = "Admin"
+        CType(Me.dataadmin, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents tbidadmin As System.Windows.Forms.TextBox
+    Friend WithEvents tbusername As System.Windows.Forms.TextBox
+    Friend WithEvents tbpassword As System.Windows.Forms.TextBox
+    Friend WithEvents tbalamat As System.Windows.Forms.TextBox
+    Friend WithEvents tbnotelp As System.Windows.Forms.TextBox
+    Friend WithEvents bttambah As System.Windows.Forms.Button
+    Friend WithEvents btedit As System.Windows.Forms.Button
+    Friend WithEvents bthapus As System.Windows.Forms.Button
+    Friend WithEvents dataadmin As System.Windows.Forms.DataGridView
+End Class
