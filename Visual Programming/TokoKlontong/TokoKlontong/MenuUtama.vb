d4e0586fff6e2b88fae71395b285560b005f5dde
Trihayu <trihayu26@gmail.com>
2020-01-16 11:00:28 +0700

    th mengedit form supplier

--- Visual Programming/TokoKlontong/TokoKlontong/MenuUtama.vb
+++ Visual Programming/TokoKlontong/TokoKlontong/MenuUtama.vb
@@ -1,3 +1,7 @@
-﻿Public Class MenuUtama
-
+﻿Imports TokoKlontong.Koneksi
+Imports MySql.Data.MySqlClient
+Public Class MenuUtama
+    Private Sub btsupplier_Click(sender As Object, e As EventArgs) Handles btsupplier.Click
+        frmSupplier.Show()
+    End Sub
 End Class
