1477b2af3e2cffdf98b8e98a20e223780454c729
dinaayunitap <ayunitadina492@gmail.com>
2020-01-18 14:10:35 +0700

    Dina menambahkan script admin

--- Visual Programming/TokoKlontong/TokoKlontong/admin.Designer.vb
+++ Visual Programming/TokoKlontong/TokoKlontong/admin.Designer.vb
@@ -31,154 +31,201 @@ Partial Class admin
         Me.tbusername = New System.Windows.Forms.TextBox()
         Me.tbpassword = New System.Windows.Forms.TextBox()
         Me.tbalamat = New System.Windows.Forms.TextBox()
-        Me.tbnotelp = New System.Windows.Forms.TextBox()
-        Me.bttambah = New System.Windows.Forms.Button()
-        Me.btedit = New System.Windows.Forms.Button()
-        Me.bthapus = New System.Windows.Forms.Button()
+        Me.tbnohp = New System.Windows.Forms.TextBox()
         Me.dataadmin = New System.Windows.Forms.DataGridView()
+        Me.btdelete = New System.Windows.Forms.Button()
+        Me.btedit = New System.Windows.Forms.Button()
+        Me.bttambah = New System.Windows.Forms.Button()
+        Me.tbjabatan = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.cbjk = New System.Windows.Forms.ComboBox()
         CType(Me.dataadmin, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.ForeColor = System.Drawing.SystemColors.HotTrack
-        Me.Label1.Location = New System.Drawing.Point(12, 56)
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label1.Location = New System.Drawing.Point(9, 4)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(65, 15)
+        Me.Label1.Size = New System.Drawing.Size(57, 15)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "ID Admin"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.ForeColor = System.Drawing.SystemColors.HotTrack
-        Me.Label2.Location = New System.Drawing.Point(12, 81)
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label2.Location = New System.Drawing.Point(9, 29)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(79, 15)
+        Me.Label2.Size = New System.Drawing.Size(70, 15)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "User Name"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.ForeColor = System.Drawing.SystemColors.HotTrack
-        Me.Label3.Location = New System.Drawing.Point(12, 110)
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label3.Location = New System.Drawing.Point(9, 58)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(69, 15)
+        Me.Label3.Size = New System.Drawing.Size(61, 15)
         Me.Label3.TabIndex = 2
         Me.Label3.Text = "Password"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.ForeColor = System.Drawing.SystemColors.HotTrack
-        Me.Label4.Location = New System.Drawing.Point(12, 137)
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label4.Location = New System.Drawing.Point(9, 85)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(51, 15)
+        Me.Label4.Size = New System.Drawing.Size(45, 15)
         Me.Label4.TabIndex = 3
         Me.Label4.Text = "Alamat"
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.ForeColor = System.Drawing.SystemColors.HotTrack
-        Me.Label5.Location = New System.Drawing.Point(12, 201)
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label5.Location = New System.Drawing.Point(9, 149)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(73, 15)
+        Me.Label5.Size = New System.Drawing.Size(64, 15)
         Me.Label5.TabIndex = 4
         Me.Label5.Text = "No Telpon"
         '
         'tbidadmin
         '
-        Me.tbidadmin.Location = New System.Drawing.Point(105, 55)
+        Me.tbidadmin.Location = New System.Drawing.Point(102, 3)
         Me.tbidadmin.Name = "tbidadmin"
         Me.tbidadmin.Size = New System.Drawing.Size(266, 20)
         Me.tbidadmin.TabIndex = 5
         '
         'tbusername
         '
-        Me.tbusername.Location = New System.Drawing.Point(105, 80)
+        Me.tbusername.Location = New System.Drawing.Point(102, 28)
         Me.tbusername.Name = "tbusername"
         Me.tbusername.Size = New System.Drawing.Size(266, 20)
         Me.tbusername.TabIndex = 6
         '
         'tbpassword
         '
-        Me.tbpassword.Location = New System.Drawing.Point(105, 109)
+        Me.tbpassword.Location = New System.Drawing.Point(102, 57)
         Me.tbpassword.Name = "tbpassword"
         Me.tbpassword.Size = New System.Drawing.Size(266, 20)
         Me.tbpassword.TabIndex = 7
         '
         'tbalamat
         '
-        Me.tbalamat.Location = New System.Drawing.Point(105, 136)
+        Me.tbalamat.Location = New System.Drawing.Point(102, 84)
         Me.tbalamat.Multiline = True
         Me.tbalamat.Name = "tbalamat"
         Me.tbalamat.Size = New System.Drawing.Size(266, 56)
         Me.tbalamat.TabIndex = 8
         '
-        'tbnotelp
+        'tbnohp
         '
-        Me.tbnotelp.Location = New System.Drawing.Point(105, 198)
-        Me.tbnotelp.Name = "tbnotelp"
-        Me.tbnotelp.Size = New System.Drawing.Size(266, 20)
-        Me.tbnotelp.TabIndex = 9
-        '
-        'bttambah
-        '
-        Me.bttambah.Location = New System.Drawing.Point(393, 81)
-        Me.bttambah.Name = "bttambah"
-        Me.bttambah.Size = New System.Drawing.Size(75, 23)
-        Me.bttambah.TabIndex = 10
-        Me.bttambah.Text = "&Tambah"
-        Me.bttambah.UseVisualStyleBackColor = True
-        '
-        'btedit
-        '
-        Me.btedit.Location = New System.Drawing.Point(393, 129)
-        Me.btedit.Name = "btedit"
-        Me.btedit.Size = New System.Drawing.Size(75, 23)
-        Me.btedit.TabIndex = 11
-        Me.btedit.Text = "&Edit"
-        Me.btedit.UseVisualStyleBackColor = True
-        '
-        'bthapus
-        '
-        Me.bthapus.Location = New System.Drawing.Point(393, 181)
-        Me.bthapus.Name = "bthapus"
-        Me.bthapus.Size = New System.Drawing.Size(75, 23)
-        Me.bthapus.TabIndex = 12
-        Me.bthapus.Text = "&Delete"
-        Me.bthapus.UseVisualStyleBackColor = True
+        Me.tbnohp.Location = New System.Drawing.Point(102, 146)
+        Me.tbnohp.Name = "tbnohp"
+        Me.tbnohp.Size = New System.Drawing.Size(266, 20)
+        Me.tbnohp.TabIndex = 9
         '
         'dataadmin
         '
         Me.dataadmin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dataadmin.Location = New System.Drawing.Point(15, 253)
+        Me.dataadmin.Location = New System.Drawing.Point(12, 224)
         Me.dataadmin.Name = "dataadmin"
         Me.dataadmin.Size = New System.Drawing.Size(559, 150)
         Me.dataadmin.TabIndex = 13
+        '
+        'btdelete
+        '
+        Me.btdelete.Location = New System.Drawing.Point(496, 113)
+        Me.btdelete.Name = "btdelete"
+        Me.btdelete.Size = New System.Drawing.Size(75, 44)
+        Me.btdelete.TabIndex = 14
+        Me.btdelete.Text = "&Delete"
+        Me.btdelete.UseVisualStyleBackColor = True
+        '
+        'btedit
+        '
+        Me.btedit.Location = New System.Drawing.Point(449, 172)
+        Me.btedit.Name = "btedit"
+        Me.btedit.Size = New System.Drawing.Size(75, 44)
+        Me.btedit.TabIndex = 15
+        Me.btedit.Text = "&Edit"
+        Me.btedit.UseVisualStyleBackColor = True
+        '
+        'bttambah
+        '
+        Me.bttambah.Location = New System.Drawing.Point(400, 113)
+        Me.bttambah.Name = "bttambah"
+        Me.bttambah.Size = New System.Drawing.Size(75, 45)
+        Me.bttambah.TabIndex = 16
+        Me.bttambah.Text = "&Tambah"
+        Me.bttambah.UseVisualStyleBackColor = True
+        '
+        'tbjabatan
+        '
+        Me.tbjabatan.Location = New System.Drawing.Point(102, 198)
+        Me.tbjabatan.Name = "tbjabatan"
+        Me.tbjabatan.Size = New System.Drawing.Size(266, 20)
+        Me.tbjabatan.TabIndex = 18
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label6.Location = New System.Drawing.Point(6, 177)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(85, 15)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "Jenis Kelamin"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label7.Location = New System.Drawing.Point(6, 203)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(51, 15)
+        Me.Label7.TabIndex = 4
+        Me.Label7.Text = "Jabatan"
+        '
+        'cbjk
+        '
+        Me.cbjk.FormattingEnabled = True
+        Me.cbjk.Items.AddRange(New Object() {"Laki - Laki", "Perempuan"})
+        Me.cbjk.Location = New System.Drawing.Point(102, 171)
+        Me.cbjk.Name = "cbjk"
+        Me.cbjk.Size = New System.Drawing.Size(266, 21)
+        Me.cbjk.TabIndex = 19
+        Me.cbjk.Text = "----- Pilih -----"
         '
         'admin
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(589, 413)
-        Me.Controls.Add(Me.dataadmin)
-        Me.Controls.Add(Me.bthapus)
+        Me.Controls.Add(Me.cbjk)
+        Me.Controls.Add(Me.tbjabatan)
+        Me.Controls.Add(Me.btdelete)
         Me.Controls.Add(Me.btedit)
         Me.Controls.Add(Me.bttambah)
-        Me.Controls.Add(Me.tbnotelp)
+        Me.Controls.Add(Me.dataadmin)
+        Me.Controls.Add(Me.tbnohp)
         Me.Controls.Add(Me.tbalamat)
         Me.Controls.Add(Me.tbpassword)
         Me.Controls.Add(Me.tbusername)
         Me.Controls.Add(Me.tbidadmin)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
@@ -200,9 +247,13 @@ Partial Class admin
     Friend WithEvents tbusername As System.Windows.Forms.TextBox
     Friend WithEvents tbpassword As System.Windows.Forms.TextBox
     Friend WithEvents tbalamat As System.Windows.Forms.TextBox
-    Friend WithEvents tbnotelp As System.Windows.Forms.TextBox
-    Friend WithEvents bttambah As System.Windows.Forms.Button
-    Friend WithEvents btedit As System.Windows.Forms.Button
-    Friend WithEvents bthapus As System.Windows.Forms.Button
+    Friend WithEvents tbnohp As System.Windows.Forms.TextBox
     Friend WithEvents dataadmin As System.Windows.Forms.DataGridView
+    Friend WithEvents btdelete As System.Windows.Forms.Button
+    Friend WithEvents btedit As System.Windows.Forms.Button
+    Friend WithEvents bttambah As System.Windows.Forms.Button
+    Friend WithEvents tbjabatan As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents cbjk As System.Windows.Forms.ComboBox
 End Class
--- Visual Programming/TokoKlontong/TokoKlontong/admin.vb
+++ Visual Programming/TokoKlontong/TokoKlontong/admin.vb
@@ -1,3 +1,213 @@
-﻿Public Class admin
+﻿Imports TokoKlontong.Koneksi
+Imports MySql.Data.MySqlClient
+Public Class admin
 
+    Private Sub statuspembersih()
+        tbidadmin.Text = ""
+        tbusername.Text = ""
+        tbpassword.Text = ""
+        tbalamat.Text = ""
+        tbnohp.Text = ""
+        cbjk.Text = ""
+        tbjabatan.Text = ""
+    End Sub
+
+    Private Sub tampildata()
+        Dim dt As DataTable
+        Dim adapter As MySqlDataAdapter
+        Dim query As String
+        Dim data As Integer
+
+        query = "select id_admin, username, password, alamat, no_telp, jk, jabatan from admin"
+        adapter = New MySqlDataAdapter(query, conn)
+        dt = New DataTable
+        data = adapter.Fill(dt)
+
+        If data > 0 Then
+            dataadmin.DataSource = dt
+            dataadmin.AutoSizeColumnsMode = DataGridViewAutoSizeColumnMode.Fill
+            dataadmin.Columns(0).HeaderText = "Id Admin"
+            dataadmin.Columns(1).HeaderText = "Username"
+            dataadmin.Columns(2).HeaderText = "Password"
+            dataadmin.Columns(3).HeaderText = "Alamat"
+            dataadmin.Columns(4).HeaderText = "No Hp"
+            dataadmin.Columns(5).HeaderText = "Jenis Kelamin"
+            dataadmin.Columns(6).HeaderText = "Jabatan"
+
+        Else
+            dataadmin.DataSource = Nothing
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If (conn.State) <> ConnectionState.Closed Then conn.Close()
+        KoneksiBuka()
+
+        CMD = New MySqlCommand(str, conn)
+        CMD.ExecuteNonQuery()
+
+        tampildata()
+
+        statusInput(False, False, False, False, False, False, False)
+        statustombol(True, False, False)
+
+    End Sub
+
+    Private Sub statusInput(id_adm As Boolean, username As Boolean, password As Boolean, alamat As Boolean, no_hp As Boolean, jk As Boolean, jabatan As Boolean)
+        tbidadmin.Enabled = id_adm
+        tbusername.Enabled = username
+        tbpassword.Enabled = password
+        tbalamat.Enabled = alamat
+        tbnohp.Enabled = no_hp
+        cbjk.Enabled = jk
+        tbjabatan.Enabled = jabatan
+    End Sub
+    Private Sub statustombol(tambah As Boolean, ubah As Boolean, hapus As Boolean)
+        btedit.Enabled = ubah
+        btdelete.Enabled = hapus
+        bttambah.Enabled = tambah
+    End Sub
+
+
+    Private Sub bttambah_Click(sender As Object, e As EventArgs) Handles bttambah.Click
+        If (conn.State) <> ConnectionState.Closed Then conn.Close()
+        KoneksiBuka()
+        If bttambah.Text = "&Tambah" Then
+            statusInput(True, True, True, True, True, True, True)
+            statustombol(True, False, True)
+            btdelete.Text = "Batal"
+            bttambah.Text = "Simpan"
+        ElseIf bttambah.Text = "Simpan" Then
+            If tbidadmin.Text = "" Then
+                MsgBox("Id Admin Harus diisi", MsgBoxStyle.Information, "Informasi")
+                tbidadmin.Focus()
+                Exit Sub
+            ElseIf tbusername.Text = "" Then
+                MsgBox("Username Harus diisi", MsgBoxStyle.Information, "Informasi")
+                tbusername.Focus()
+                Exit Sub
+            ElseIf tbpassword.Text = "" Then
+                MsgBox("Password Mu harus diisi", MsgBoxStyle.Information, "Informasi")
+                tbpassword.Focus()
+                Exit Sub
+            ElseIf tbalamat.Text = "" Then
+                MsgBox("Alamat Harus diisi", MsgBoxStyle.Information, "Informasi")
+                tbalamat.Focus()
+                Exit Sub
+            ElseIf tbnohp.Text = "" Then
+                MsgBox("No Handphone Harus Diisi", MsgBoxStyle.Information, "Informasi")
+                tbnohp.Focus()
+                Exit Sub
+            ElseIf cbjk.Text = "" Then
+                MsgBox("Jenis Kelamin Harus dipilih", MsgBoxStyle.Information, "Information")
+                cbjk.Focus()
+                Exit Sub
+            ElseIf tbjabatan.Text = "" Then
+                MsgBox("Jabatan Harus Diisi", MsgBoxStyle.Information, "Information")
+                tbjabatan.Focus()
+                Exit Sub
+            End If
+            'conn.Open()
+            Try
+                Dim qinsert As String
+                CMD.CommandType = CommandType.Text
+                qinsert = "insert into admin(id_admin, username, password, alamat, no_telp, jk, jabatan)"
+                qinsert = qinsert & "values('" & tbidadmin.Text & "', '" & tbusername.Text & "', '" & tbpassword.Text & "', '" & tbalamat.Text & "', '" & tbnohp.Text & "', '" & cbjk.Text & "', '" & tbjabatan.Text & "')"
+                CMD.CommandType = CommandType.Text
+                CMD.CommandText = qinsert
+                CMD.Connection = conn
+                CMD.ExecuteNonQuery()
+
+                tampildata()
+            Catch ex As Exception
+                MsgBox("Gagal Simpan " + ex.Message, MsgBoxStyle.Critical, "Terjadi Kesalahan")
+
+            End Try
+            bttambah.Text = "&Tambah"
+            statusInput(False, False, False, False, False, False, False)
+            statustombol(True, False, False)
+            statuspembersih()
+        End If
+
+        'conn.Close()
+    End Sub
+
+    Private Sub btdelete_Click(sender As Object, e As EventArgs) Handles btdelete.Click
+        If (conn.State) <> ConnectionState.Closed Then conn.Close()
+        KoneksiBuka()
+        If btdelete.Text = "&Hapus" Then
+            Dim yakinhapus As MsgBoxResult = MessageBox.Show("Yakin mau dihapus", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+            If yakinhapus = vbYes Then
+                Try
+                    Dim qDelete As String
+                    qDelete = "delete from admin where id_admin ='" & tbidadmin.Text & "'"
+                    CMD.CommandType = CommandType.Text
+                    CMD.CommandText = qDelete
+                    CMD.Connection = conn
+                    CMD.ExecuteNonQuery()
+
+                    tampildata()
+                    ''statuspembersih()
+                Catch ex As Exception
+                    MsgBox("Gagal hapus Data " + ex.Message, MsgBoxStyle.Critical, "Terjadi Kesalahan")
+                End Try
+                statuspembersih()
+                statustombol(True, False, False)
+            End If
+        End If
+
+        If btdelete.Text = "Batal" Then
+            btdelete.Text = "&Hapus"
+            bttambah.Text = "&Tambah"
+            statusInput(False, False, False, False, False, False, False)
+            statustombol(True, False, False)
+            statuspembersih()
+        End If
+    End Sub
+
+    Private Sub dataadmin_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dataadmin.CellClick
+        Dim i As Integer
+        i = Me.dataadmin.CurrentRow.Index
+        With dataadmin.Rows.Item(i)
+            tbidadmin.Text = .Cells(0).Value
+            tbusername.Text = .Cells(1).Value
+            tbpassword.Text = .Cells(2).Value
+            tbnohp.Text = .Cells(3).Value
+            cbjk.Text = .Cells(4).Value
+            tbjabatan.Text = .Cells(5).Value
+        End With
+        statustombol(True, True, True)
+        btdelete.Text = "&Hapus"
+        ''bttambah.Text = "Batal"
+        statusInput(False, False, False, False, False, False, False)
+    End Sub
+
+    Private Sub btedit_Click(sender As Object, e As EventArgs) Handles btedit.Click
+        If (conn.State) <> ConnectionState.Closed Then conn.Close()
+        KoneksiBuka()
+        If btedit.Text = "&Edit" Then
+            statustombol(False, True, False)
+            btedit.Text = "&Simpan"
+            statusInput(False, True, True, True, True, True, True)
+        ElseIf btedit.Text = "&Simpan" Then
+            Try
+                Dim qUpdate As String
+                qUpdate = "update admin set username='" & tbusername.Text & "', password='" & tbpassword.Text & "', alamat='" & tbalamat.Text & "', no_telp='" & tbnohp.Text & "', jk='" & cbjk.Text & "', jabatan='" & tbjabatan.Text & "' where id_admin='" & tbidadmin.Text & "'"
+                CMD.CommandType = CommandType.Text
+                CMD.CommandText = qUpdate
+                CMD.Connection = conn
+                CMD.ExecuteNonQuery()
+
+                MsgBox("Data berhasil di Update", MsgBoxStyle.Information, "Informasi")
+                tampildata()
+
+                btedit.Text = "&Edit"
+            Catch ex As Exception
+                MsgBox("Gagal update Data " + ex.Message, MsgBoxStyle.Critical, "Terjadi Kesalahan")
+            End Try
+            statusInput(False, False, False, False, False, False, False)
+            statustombol(True, False, False)
+            statuspembersih()
+        End If
+    End Sub
 End Class
