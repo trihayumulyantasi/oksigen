ac66008f5c7c164bcb4dd042e5ae00f57c80c3e7
Dina <ayunitadina492@gmail.com>
2020-01-26 12:56:03 +0700

    edit login

--- Visual Programming/TokoKlontong/TokoKlontong/Login.Designer.vb
+++ Visual Programming/TokoKlontong/TokoKlontong/Login.Designer.vb
@@ -23,8 +23,6 @@ Partial Class Login
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.lbusername = New System.Windows.Forms.Label()
-        Me.lbpassword = New System.Windows.Forms.Label()
         Me.tbusername = New System.Windows.Forms.TextBox()
         Me.tbpassword = New System.Windows.Forms.TextBox()
         Me.btlogin = New System.Windows.Forms.Button()
@@ -35,49 +33,45 @@ Partial Class Login
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(41, 25)
+        Me.Label1.BackColor = System.Drawing.SystemColors.Desktop
+        Me.Label1.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.GrayText
+        Me.Label1.Location = New System.Drawing.Point(94, 111)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(308, 20)
+        Me.Label1.Size = New System.Drawing.Size(192, 20)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "SILAHKAN LOGIN TERLEBIH DAHULLU"
-        '
-        'lbusername
-        '
-        Me.lbusername.AutoSize = True
-        Me.lbusername.Location = New System.Drawing.Point(41, 69)
-        Me.lbusername.Name = "lbusername"
-        Me.lbusername.Size = New System.Drawing.Size(55, 13)
-        Me.lbusername.TabIndex = 1
-        Me.lbusername.Text = "Username"
-        '
-        'lbpassword
-        '
-        Me.lbpassword.AutoSize = True
-        Me.lbpassword.Location = New System.Drawing.Point(43, 106)
-        Me.lbpassword.Name = "lbpassword"
-        Me.lbpassword.Size = New System.Drawing.Size(53, 13)
-        Me.lbpassword.TabIndex = 2
-        Me.lbpassword.Text = "Password"
+        Me.Label1.Text = "Silahkan Login Terlebih Dahulu"
         '
         'tbusername
         '
-        Me.tbusername.Location = New System.Drawing.Point(147, 66)
+        Me.tbusername.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.tbusername.Location = New System.Drawing.Point(110, 167)
+        Me.tbusername.Multiline = True
         Me.tbusername.Name = "tbusername"
-        Me.tbusername.Size = New System.Drawing.Size(216, 20)
+        Me.tbusername.Size = New System.Drawing.Size(169, 27)
         Me.tbusername.TabIndex = 3
+        Me.tbusername.Text = "username"
+        Me.tbusername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.tbusername.WordWrap = False
         '
         'tbpassword
         '
-        Me.tbpassword.Location = New System.Drawing.Point(147, 103)
+        Me.tbpassword.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.tbpassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.tbpassword.Location = New System.Drawing.Point(110, 231)
+        Me.tbpassword.Multiline = True
         Me.tbpassword.Name = "tbpassword"
         Me.tbpassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.tbpassword.Size = New System.Drawing.Size(216, 20)
+        Me.tbpassword.Size = New System.Drawing.Size(169, 30)
         Me.tbpassword.TabIndex = 4
+        Me.tbpassword.Text = "password"
+        Me.tbpassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.tbpassword.WordWrap = False
         '
         'btlogin
         '
-        Me.btlogin.Location = New System.Drawing.Point(288, 177)
+        Me.btlogin.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btlogin.Location = New System.Drawing.Point(204, 319)
         Me.btlogin.Name = "btlogin"
         Me.btlogin.Size = New System.Drawing.Size(75, 23)
         Me.btlogin.TabIndex = 5
@@ -86,7 +80,7 @@ Partial Class Login
         '
         'btbatal
         '
-        Me.btbatal.Location = New System.Drawing.Point(147, 177)
+        Me.btbatal.Location = New System.Drawing.Point(110, 319)
         Me.btbatal.Name = "btbatal"
         Me.btbatal.Size = New System.Drawing.Size(75, 23)
         Me.btbatal.TabIndex = 6
@@ -96,36 +90,38 @@ Partial Class Login
         'CheckBox1
         '
         Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.BackColor = System.Drawing.SystemColors.Desktop
         Me.CheckBox1.Font = New System.Drawing.Font("Gadugi", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox1.Location = New System.Drawing.Point(147, 138)
+        Me.CheckBox1.ForeColor = System.Drawing.SystemColors.GrayText
+        Me.CheckBox1.Location = New System.Drawing.Point(110, 276)
         Me.CheckBox1.Name = "CheckBox1"
         Me.CheckBox1.Size = New System.Drawing.Size(146, 20)
         Me.CheckBox1.TabIndex = 30
         Me.CheckBox1.Text = "Tampilkan Password"
-        Me.CheckBox1.UseVisualStyleBackColor = True
+        Me.CheckBox1.UseVisualStyleBackColor = False
         '
         'Login
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(407, 230)
+        Me.BackColor = System.Drawing.SystemColors.Desktop
+        Me.BackgroundImage = Global.TokoKlontong.My.Resources.Resources.LoginOksigen2
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.ClientSize = New System.Drawing.Size(386, 390)
+        Me.ControlBox = False
         Me.Controls.Add(Me.CheckBox1)
         Me.Controls.Add(Me.btbatal)
         Me.Controls.Add(Me.btlogin)
         Me.Controls.Add(Me.tbpassword)
         Me.Controls.Add(Me.tbusername)
-        Me.Controls.Add(Me.lbpassword)
-        Me.Controls.Add(Me.lbusername)
         Me.Controls.Add(Me.Label1)
+        Me.DoubleBuffered = True
         Me.Name = "Login"
-        Me.Text = "Login"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents lbusername As System.Windows.Forms.Label
-    Friend WithEvents lbpassword As System.Windows.Forms.Label
     Friend WithEvents tbusername As System.Windows.Forms.TextBox
     Friend WithEvents tbpassword As System.Windows.Forms.TextBox
     Friend WithEvents btlogin As System.Windows.Forms.Button
